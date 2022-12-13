c5ac3701f8c522fa80894bf1eae35aac9cb279da
Hugo Maxted <hugo@maxtedfamily.net>
2022-12-13 10:47:41 +0000

    Imported code

---
+++ index.js
@@ -0,0 +1,57 @@
+const { Client, GatewayIntentBits, Partials } = require("discord.js");
+const { joinVoiceChannel, createAudioPlayer, createAudioResource, entersState, StreamType, AudioPlayerStatus, VoiceConnectionStatus } = require("@discordjs/voice");
+
+const client = new Client({
+  partials: [Partials.Channel, Partials.Message, Partials.Reaction],
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildVoiceStates,
+    GatewayIntentBits.DirectMessages,
+    GatewayIntentBits.DirectMessageTyping,
+  ],
+});
+
+client.login("MTA0ODE1ODQzNzY4MDM0OTE4NA.G0iSww.5rlRjyiqPCIdVltdj0eTihuwvBpwsUCcP4tgUE");
+
+client.once("ready", () => {
+  console.log("Ready!");
+});
+
+client.on("error", console.error);
+client.on("warn", console.warn);
+
+const player = createAudioPlayer();
+
+client.on("messageCreate", async function (message) {
+  console.log(message.author.username + " said: " + message.content);
+
+  if (message.content.startsWith("&play")) {
+    const voiceChannel = message.member.voice.channel;
+
+    if (!voiceChannel) return message.reply("You must be in a voice channel to be a miner.");
+
+    const connection = joinVoiceChannel({
+      channelId: voiceChannel.id,
+      guildId: voiceChannel.guild.id,
+      adapterCreator: voiceChannel.guild.voiceAdapterCreator,
+    });
+
+    connection.subscribe(player);
+
+    player.play(
+      createAudioResource("./bop.mp3", {
+        inputType: StreamType.Arbitrary,
+      })
+    );
+
+    entersState(player, AudioPlayerStatus.Playing, 5e3);
+
+    connection.on(VoiceConnectionStatus.Ready, () => {
+      message.reply("ok boss");
+      console.log("Starting Bopping");
+    });
+  }
+});
